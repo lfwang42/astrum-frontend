10efc478d3f2b01eff64e06170b7db2966002c94
Lanney Wang <lfwang42@gmail.com>
2023-12-28 02:43:22 -0500

    fixed missing stat, switch leaderboard page to customtable

--- app/[locale]/leaderboard/[calc_id]/page.tsx
+++ app/[locale]/leaderboard/[calc_id]/page.tsx
@@ -1,14 +1,37 @@
-import Navbar from "../../../../components/navbar";
-import { LeaderboardRow, columns } from './columns';
+
+'use client'
 import { DataTable } from "./data-table"
 import axios from 'axios'
-import react, { useEffect, useState } from 'react'
+import react, { useEffect, useMemo, useState } from 'react'
 import { AvatarCategory } from '../../../types';
-import Image from 'next/image';
-import Link from 'next/link';
 import { ConeDisplay } from "@/components/ConeDisplay";
-import { Span } from "next/dist/trace";
 import { getAPIURL } from "@/lib/utils";
+import { createColumnHelper, ColumnDef } from "@tanstack/react-table"
+import { CoreStats, SetInfo } from '../../../types';
+import { SetDisplay } from '../../../../components/SetDisplay/index';
+import { getRegion } from '../../../../lib/utils';
+import { CustomTable } from "@/components/CustomTable";
+import React from "react";
+import useSWR from "swr";
+
+
+export type LeaderboardRow = {
+  region: string
+  rank: number
+  bid: number
+  uid: number
+  score: number
+  crit_value: number
+  stats: CoreStats
+  nickname: string
+  platform: string
+  calc_name: string
+  sets: SetInfo[]
+}
+
+const columnHelper = createColumnHelper<LeaderboardRow>()
+
+
 
 async function getData(calc_id: number): Promise<LeaderboardRow[]> {
   const res =  await axios.get(getAPIURL(`/api/leaderboard`), { params: {calc_id: calc_id } })
@@ -32,8 +55,17 @@ function getName(cats: AvatarCategory[], calc_id: number): string {
   return ""
 }
 
+function fetcher(params: any) {
+  const [url, query] = params;
+  const res = axios.get(url, query).then(res => res.data)
+  // axios.get(url, query).
+  //   then(res => { return res.data })
+  return res
+}
+
+
 //params: {avatar_id: 1104}
-export default async function Leaderboard({ params }: { params: { calc_id: number }}) {
+export default function Leaderboard({ params }: { params: { calc_id: number }}) {
     // const [data, setData] = useState<LeaderboardRow[]>();
     // useEffect(() => {
     //   async function getData() {
@@ -42,20 +74,77 @@ export default async function Leaderboard({ params }: { params: { calc_id: numbe
     //   }
     //   getData();
     // }, [])
-    const data = await getData(params.calc_id)
-    const calcs = await getCalcs(params.calc_id)
-    const name = getName(calcs, +params.calc_id)
+    const leaderboardData = useSWR([getAPIURL('/api/leaderboard'), {params: {calc_id: params.calc_id}}] , fetcher) 
+    const calcs = useSWR([getAPIURL('/api/categories'), {params: {avatar_id: params.calc_id.toString().slice(0, 4)}}] , fetcher) 
+    // const data = await getData(params.calc_id)
+    // const calcs = await getCalcs(params.calc_id)
+    // const name = getName(calcs, +params.calc_id)
+    // const columns:  ColumnDef<LeaderboardRow>[] = ['
+    const columns = useMemo<ColumnDef<LeaderboardRow>[]>(() => [
+
+      {
+        accessorKey: "rank",
+        header: "#",
+      },
+      {
+        header: "Region",
+        accessorKey: "region"
+      },
+      {
+        header: "Owner",
+        accessorKey: "nickname",
+        cell: ({ row } ) => (
+          <a href={`../profile/${row.original.uid}`}>{row.original.nickname}</a>
+        ),
+      },
+      {
+        header: "Sets",
+        accessorKey: "set",
+        cell: ({ row} ) => (
+          <SetDisplay sets={row.original.sets} />
+        ),
+      },
+      {
+        header: "Crit Value",
+        accessorFn: row => (row.crit_value * 100).toFixed(1)
+      },
+      {
+        header: "HP",
+        accessorFn: row => (row.stats.maxHP).toFixed(0)
+      },
+      {
+        header: "ATK",
+        accessorFn: row => (row.stats.atk).toFixed(0)
+      },
+      {
+        header: "DEF",
+        accessorFn: row => (row.stats.def).toFixed(0)
+      },
+      {
+        header: "SPD",
+        accessorFn: row => (row.stats.spd).toFixed(0)
+      },
+      {
+        accessorKey: "score",
+        header: calcs.isLoading ? '??' : getName(calcs.data, params.calc_id),
+        cell: ({row}) => {
+          return <span>{row.original.score.toFixed(0)}</span>
+        }
+      },
+    
+    ], [])
     return (
       <>
         <div className="container mx-auto py-10">
           <div className="flex py-10">
             {
-              calcs.map((category) => {
+              calcs.isLoading ? <></> : 
+              calcs.data.map((category: any) => {
                 return (
                   <div key={category.name}>
                     <span className="my-4 text-lg">{category.name}</span>
                       <div className="flex justify-start whitespace-nowrap gap-5">
-                      {category.calculations.map((calc) => {
+                      {category.calculations.map((calc: any) => {
                         // if (calc.calc_id === +params.calc_id) console.log('same')
                         return <div key={calc.calc_id} className={calc.calc_id === +params.calc_id ? "p-2 bg-slate-600" : "p-2"}><a href={`${calc.calc_id}`}><ConeDisplay icon={calc.icon} imposition={calc.rank}/></a></div>
                       })}
@@ -65,7 +154,7 @@ export default async function Leaderboard({ params }: { params: { calc_id: numbe
               })
             }
           </div>
-          <DataTable columns={columns} data={data} score_name={name} />
+          <CustomTable columns={columns} data={leaderboardData.data} isLoading={leaderboardData.isLoading}/>
         </div>
       </>
     );
