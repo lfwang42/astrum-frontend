48289a0e7fc3db4adebe1a3f9ee28a39316e8e52
Lanney Wang <lfwang42@gmail.com>
2024-05-07 02:36:28 -0400

    reformatting

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,61 +1,57 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
 
-import '../globals.css'
-import Navbar from '../../components/Navbar/index';
-import Footer from '@/components/Footer';
-const inter = Inter({ subsets: ['latin'] })
+import "../globals.css";
+import Navbar from "../../components/Navbar/index";
+import Footer from "@/components/Footer";
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: 'Astrum',
-  description: 'Created by AkoDako',
-}
+  title: "Astrum",
+  description: "Created by AkoDako",
+};
 
 export function generateStaticParams() {
-  return [{locale: 'en'}, {locale: 'jp'}];
+  return [{ locale: "en" }, { locale: "jp" }];
 }
- 
 
 export default async function RootLayout({
-  children, params: {locale}
+  children,
+  params: { locale },
 }: {
-  children: React.ReactNode,
-  params: {locale: string}
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
-
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
   } catch (error) {
-    console.log('error')
+    console.log("error");
     notFound();
   }
   return (
     <html lang={locale} className={inter.className}>
-
       <head>
         <link
-            rel="icon"
-            href="/icon?<generated>"
-            type="image/<generated>"
-            sizes="<generated>"
-          />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6217820332998691"
-          crossOrigin="anonymous">
-        </script>
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6217820332998691"
+          crossOrigin="anonymous"
+        ></script>
       </head>
-    
-        <body className="min-h-screen items-center bg-slate-800 text-slate-50 mb-5">
-          <NextIntlClientProvider locale={locale} messages={messages}>
-            <Navbar locale={locale}/>
-            {children}
-          </NextIntlClientProvider>
-          <Footer />
-        </body>
-
-
-
+      <body className="min-h-screen items-center bg-slate-800 text-slate-50 mb-5">
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Navbar locale={locale} />
+          {children}
+        </NextIntlClientProvider>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
