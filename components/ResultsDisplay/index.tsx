d57466f78b96105bb84e790e8b4cd155d6975b68
Lanney Wang <lfwang42@gmail.com>
2024-05-10 01:43:07 -0400

    results display

--- components/ResultsDisplay/index.tsx
+++ components/ResultsDisplay/index.tsx
@@ -23,39 +23,53 @@ export const ResultsDisplay: React.FC<ResultsProps> = ({ user }) => {
             Math.ceil((best.rank / best.outof) * 100)
           );
           const avatar_ids = result.avatar_id.toString().match(/.{1,4}/g);
+
           return (
             <Link
               key={`result-${user.uid}-${result.avatar_id}`}
               href={`/leaderboard/${best.calc_id}/`}
             >
-              <div className="justify-center align-middle text-center bg-slate-700 py-[4px] px-[6px] hover:bg-slate-400 max-w-28 min-h-34 hover:border-sky-900 border-2 border-none">
-                <div className="flex align-middle items-center justify-center min-h-8">
+              <div className="justify-center align-middle text-center bg-slate-700 py-[1px] px-[4px] hover:bg-slate-400 max-w-28 min-h-[128px] hover:border-sky-900 border-2 border-none">
+                <div className="flex align-middle items-center justify-center min-h-6">
                   <span className="text-center text-xs items-center align-middle font-medium text-wrap">
                     {best.score_name}
                   </span>
                 </div>
-                <div className="flex justify-center gap-[2px] mb-[1px]">
+                <div className={`flex ${avatar_ids?.length == 1 ? 'flex-row justify-center' : `flex-col`} items-center min-h-[50px]`} >
+                  <div className="flex justify-center gap-[1px] mb-[1px]">
                   {avatar_ids?.map((avatar_id, index) => {
+                    const size = avatar_ids.length == 1 ? 35 : 24
+
                     return (
                       <Image
                         key={`${user.uid}-${avatar_id}-${index}`}
                         src={`https://enka.network/ui/hsr/SpriteOutput/AvatarRoundIcon/${avatar_id}.png`}
                         className=""
                         unoptimized
-                        height={28}
-                        width={28}
+                        height={size}
+                        width={size}
                         alt="character image"
                       />
                     );
                   })}
-                  <Image
-                    src={`/icon/${best.tids[0]}.png`}
-                    className="ml-1"
-                    height={30}
-                    width={30}
-                    alt="lightcone image"
-                    unoptimized
-                  />
+                  </div>
+                  <div className="flex justify-center gap-[1px]">
+                  {best.tids.map((tid, index) => {
+                    const size = avatar_ids?.length == 1 ? 35 : 24
+                    return (
+                      <Image
+                        key={`${tid}-index`}
+                        src={`/icon/${tid}.png`}
+                        className=""
+                        height={size}
+                        width={size}
+                        alt="lightcone image"
+                        unoptimized
+                      />
+                    );
+                  })}
+                  </div>
+                 
                 </div>
                 <div className="text-sm">{`Top ${topPercentage || "?"}%`}</div>
                 <span className="text-sm">
